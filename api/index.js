f681ca941cc66302fd50e6943c692c3de992027e
Koyanrush <1748341434@qq.com>
2025-12-31 18:03:32 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
-{
-  "name": "waline-server",
-  "version": "1.0.0",
-  "private": true,
-  "dependencies": {
-    "@waline/vercel": "latest"
-  }
-}
+const { Waline } = require('@waline/vercel');
+
+module.exports = Waline({
+  // 逻辑会自动读取 Vercel 的环境变量
+});
