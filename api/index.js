b523089b404024edcd8fafbde7d94b093bc133d0
Koyanrush <1748341434@qq.com>
2025-12-31 18:18:31 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const { Waline } = require('@waline/vercel');
 
-module.exports = Waline({
-  // 逻辑会自动读取 Vercel 的环境变量
+// 注意这里：最新版需要使用 .init() 方法
+module.exports = Waline.init({
+  // 保持为空，它会自动读取 Vercel 的环境变量
 });
